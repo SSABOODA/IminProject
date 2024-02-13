992f55e7e2b9b4724981abc46b39abe2739c4d6a
SSABOODA <ssaboo92@gmail.com>
2024-02-13 20:57:48 +0900

    Initial Commit

---
+++ IminProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IminProject
+//
+//  Created by 한성봉 on 2/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
